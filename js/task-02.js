a41f1d8167062184eb9d285188256d1686be9a4b
Alina Rieznik <alina.raicheva97@gmail.com>
2023-03-09 23:55:35 +0200

    add task 1 and 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,40 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// 1 option 
+// const listEl = document.querySelector('#ingredients');
+// const makeList = options => {
+
+//   const items = [];
+//   options.forEach(item => {
+//     const itemsEl = document.createElement('li');
+//     itemsEl.classList.add('item');
+//     itemsEl.textContent = `${item}`;
+//     items.push(itemsEl);
+//   });
+
+//   return items;
+// }
+
+// const elements = makeList(ingredients);
+// listEl.append(...elements);
+
+
+// 2 option
+const listEl = document.querySelector('#ingredients');
+const makeList = options => {
+  return options.map(item => {
+    const itemsEl = document.createElement('li');
+    itemsEl.classList.add('item');
+    itemsEl.textContent = `${item}`;
+    // console.log(itemsEl);
+    return itemsEl;
+  });
+}
+
+const items = makeList(ingredients);
+listEl.append(...items);
+
+
+// questions about foreach, differences with map during working process
