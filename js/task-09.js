3dd202024a68ee03bf153e9320da5c5d764e90e6
Alina Rieznik <alina.raicheva97@gmail.com>
2023-03-17 16:30:37 +0200

    add task 9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.body;
+const spanEl = bodyEl.querySelector('.color');
+const buttonEl = bodyEl.querySelector('.change-color');
+
+buttonEl.addEventListener('click', onTargetButtonClick);
+
+function onTargetButtonClick (event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = bodyEl.style.backgroundColor;
+}
+
+
