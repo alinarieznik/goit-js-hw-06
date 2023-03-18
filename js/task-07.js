55bc860d564b0bc191bb27d12751b3c8b39c86e3
Alina Rieznik <alina.raicheva97@gmail.com>
2023-03-18 14:10:15 +0200

    fix task 7

--- js/task-07.js
+++ js/task-07.js
@@ -2,6 +2,9 @@ const inputEl = document.querySelector('#font-size-control');
 const spanEl = document.querySelector('#text');
 
 inputEl.addEventListener('input', onInputChange);
+spanEl.style.fontSize = inputEl.style.fontSize;
+
+spanEl.style.fontSize = `${inputEl.value}px`;
 
 function onInputChange (event) {
     spanEl.style.fontSize = `${event.currentTarget.value}px`;
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,47 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+
+// to figure out how to do it
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+
+// const mainDivEl = document.querySelector("#controls");
+// console.log(mainDivEl);
+// const inputEl = mainDivEl.firstElementChild;
+// console.log(inputEl);
+// const buttonDestroyEl = mainDivEl.lastElementChild;
+// console.log(buttonDestroyEl);
+// const buttonCreateEl = buttonDestroyEl.previousElementSibling;
+// console.log(buttonCreateEl);
+// const boxesEl = document.querySelector("#boxes");
+// console.log(boxesEl);
+
+
+// let createdBoxes = [];
+
+// function createBoxes(amount) {
+//   for (let i = 0; i < amount; i +=1) {
+//     const boxEl = document.createElement('div');
+//     console.log(boxEl);
+//     const basicSize = 30;
+//     let boxSize = basicSize + i * 10;
+//     boxEl.style.width = `${boxSize}px`;
+//     boxEl.style.height = `${boxSize}px`;
+//     boxEl.style.backgroundColor = getRandomHexColor();
+//     createdBoxes.push(boxEl);
+//     console.log(createdBoxes);
+
+//   }
+//   boxesEl.append(...createdBoxes);
+// }
+
+// function getAmount() {
+//   const amount = Number(inputEl.value);
+//   console.log(amount);
+//   createBoxes(amount);
+// }
+
+// getAmount();
+
+
